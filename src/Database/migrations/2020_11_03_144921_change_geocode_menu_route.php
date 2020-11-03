a8a729caa7c7c9d362f4d471c65afa2cfafac6c1
gustavo.silva <gustavo.silva@codificar.com.br>
2020-11-03 14:56:37 -0300

    Change Menu Route Migration

---
+++ src/Database/migrations/2020_11_03_144921_change_geocode_menu_route.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeGeocodeMenuRoute extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $permission = Permission::where('name', 'Geolocation')->where('url', '/admin/settings/geolocation')
+        ->update(['url' => '/admin/libs/geolocation']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $permission = Permission::where('name', 'Geolocation')->where('url', '/admin/libs/geolocation')
+        ->update(['url' => '/admin/settings/geolocation']);
+    }
+}
