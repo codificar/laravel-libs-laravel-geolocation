5b26d85811e6fbfc1cfc36e5f4790e65feec9e1e
gustavo.silva <gustavo.silva@codificar.com.br>
2021-03-11 16:18:01 -0300

    Add Provider App Libs Routes

--- src/routes/app/provider.php
+++ src/routes/app/provider.php
@@ -1,12 +1,30 @@
 <?php
-Route::group(['prefix' => '/provider', 'namespace' => 'Codificar\Geolocation\Http\Controllers', 'middleware' => 'auth.provider_api:api' ], function () {  
+// Route::group(['prefix' => '/provider', 'namespace' => 'Codificar\Geolocation\Http\Controllers', 'middleware' => 'auth.provider_api:api' ], function () {  
+Route::group(['prefix' => '/provider', 'namespace' => 'Codificar\Geolocation\Http\Controllers'], function () {  
     //PLACES   
-    Route::get('/get_address_string', 'GeolocationController@getAddressByString');      
+    Route::get('/get_address_string', 'GeolocationController@getAddressByString');
+
+    //Only Services
+    Route::post('/geolocation/get_polyline', 'DirectionsController@getPolylineAndEstimateByDirectionsApi');       
+
+    //Get Geocode By PlaceId
+    Route::post('/get_place_details', 'GeolocationController@geocodeByPlaceId');
+    //Get Geocode Reverse
+    Route::post('/geolocation/get_address_by_geocode', 'GeolocationController@geocodeReverse');
+    //Get Geocode
+    Route::post('/geolocation/get_geocode_by_address', 'GeolocationController@geocode');   
 });
 
 //Only Fretes
 Route::group(['prefix' => '/api/v1/provider', 'namespace' => 'Codificar\Geolocation\Http\Controllers', 'middleware' => 'auth.provider_api:api' ], function () {  
 //Direction       
     Route::get('/geolocation/get_polyline', 'DirectionsController@getAddressByString'); 
+
+    //Get Geocode By PlaceId
+    Route::post('/get_place_details', 'GeolocationController@geocodeByPlaceId');
+    //Get Geocode Reverse
+    Route::post('/geolocation/get_address_by_geocode', 'GeolocationController@geocodeReverse');
+    //Get Geocode
+    Route::post('/geolocation/get_geocode_by_address', 'GeolocationController@geocode');  
 });
  
