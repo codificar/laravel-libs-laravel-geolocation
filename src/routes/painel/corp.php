bd31967041cdb468f6d7d2cec5bcae490ef23918
Gustavo Silva <gustavo.silva@codificar.com.br>
2021-04-15 15:30:56 -0300

    Add User And Corp geocode_reverse route

--- src/routes/painel/corp.php
+++ src/routes/painel/corp.php
@@ -2,7 +2,7 @@
 Route::group(['prefix' => '/api/v1/libs/geolocation', 'namespace' => 'Codificar\Geolocation\Http\Controllers', 'middleware' => ['auth.corp_api', 'cors']], function () {  
     Route::get('/corp/get_address_string', ['as' => 'corpAutocompleteUrl', 'uses' => 'GeolocationController@getAddressByString']);
     Route::get('/corp/geocode', ['as' => 'corpGeocodeUrl', 'uses' => 'GeolocationController@geocode']);
-
+    Route::get('/corp/geocode_reverse', ['as' => 'corpGeocodeUrlGeolocationLib', 'uses' => 'GeolocationController@geocodeReverse']);
     //Only Motoboys
     Route::get('corp/get_polyline_waypoints', array('as' => 'corpPolylineWithPoints', 'uses' => 'DirectionsController@getPolylineAndEstimateWithWayPointsApi'));  
 
--- src/routes/painel/user.php
+++ src/routes/painel/user.php
@@ -3,6 +3,7 @@
 Route::group(['prefix' => '/api/v1/libs/geolocation', 'namespace' => 'Codificar\Geolocation\Http\Controllers', 'middleware' => ['auth.user_api', 'cors']], function () {  
     Route::get('/user/get_address_string', ['as' => 'userAutocompleteUrl', 'uses' => 'GeolocationController@getAddressByString']);
     Route::get('/user/geocode', ['as' => 'userGeocodeUrl', 'uses' => 'GeolocationController@geocode']);
+    Route::get('/user/geocode_reverse', ['as' => 'userGeocodeUrlGeolocationLib', 'uses' => 'GeolocationController@geocodeReverse']);
 
     //Only Motoboys
     Route::get('/user/get_polyline_waypoints', array('as' => 'userPolylineWithPoints', 'uses' => 'DirectionsController@getPolylineAndEstimateWithWayPointsApi')); 
