44f24c674f796e20c59c5740fd7f9b3c98fe02d4
Luiz Antunes <luiz.antunes@codificar.com.br>
2021-07-16 13:35:18 -0300

    fix chamada geolocation provider

--- src/Http/Requests/PlacesFormRequest.php
+++ src/Http/Requests/PlacesFormRequest.php
@@ -15,65 +15,82 @@ use Codificar\Geolocation\Http\Rules\CheckLong;
 
 class PlacesFormRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        $this->user = \User::whereId(request()->id)->whereToken(request()->token)->first();
-        
-        return [
-            'place'     =>  ['required', 'string'],
-            'latitude'  =>  ['required', new CheckLat($this->latitude)],
-            'longitude' =>  ['required', new CheckLong($this->longitude)]
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			'place'     =>  ['required', 'string'],
+			'latitude'  =>  ['required', new CheckLat($this->latitude)],
+			'longitude' =>  ['required', new CheckLong($this->longitude)]
 		];
-    }
+	}
 
-    /**
-     * Get the error messages for the defined validation rules.
-     *
-     * @return array
-     */
-    public function messages() 
-    {
-        return [
-            'token.required'        =>  trans('geolocationTrans::geolocation.token_required'),
-            'id.required'           =>  trans('geolocationTrans::geolocation.id_required'),
-            'place.required'        =>  trans('geolocationTrans::geolocation.place_is_required'),
-        ];
-    }
+	/**
+	 * Get the error messages for the defined validation rules.
+	 *
+	 * @return array
+	 */
+	public function messages()
+	{
+		return [
+			'token.required'        =>  trans('geolocationTrans::geolocation.token_required'),
+			'id.required'           =>  trans('geolocationTrans::geolocation.id_required'),
+			'place.required'        =>  trans('geolocationTrans::geolocation.place_is_required'),
+		];
+	}
 
-    /**
-     * Caso a validação falhe, retorna os itens de erro
-     * 
-     * @return Json
-     */
-    protected function failedValidation(Validator $validator) 
-    {   
-        // Pega as mensagens de erro     
-        $error_messages = $validator->errors()->all();
+	/**
+	 * Caso a validação falhe, retorna os itens de erro
+	 * 
+	 * @return Json
+	 */
+	protected function failedValidation(Validator $validator)
+	{
+		// Pega as mensagens de erro     
+		$error_messages = $validator->errors()->all();
 
-        // Exibe os parâmetros de erro
-        throw new HttpResponseException(
-        response()->json(
-            [
-                'success' => false,
-                'error' => $error_messages[0],
-                'error_code' => \ApiErrors::REQUEST_FAILED,
-                'error_messages' => $error_messages,
-            ]
-        ));
-    }
+		// Exibe os parâmetros de erro
+		throw new HttpResponseException(
+			response()->json(
+				[
+					'success' => false,
+					'error' => $error_messages[0],
+					'error_code' => \ApiErrors::REQUEST_FAILED,
+					'error_messages' => $error_messages,
+				]
+			)
+		);
+	}
+
+	/**
+	 * Prepare the data for validation.
+	 *
+	 * @return void
+	 */
+	protected function prepareForValidation()
+	{
+		$this->user = \User::whereId($this->id)->whereToken($this->token)->first();
+
+		if ($this->type && $this->type == 'provider') {
+			$this->user = \Provider::whereId($this->id)->whereToken($this->token)->first();
+		}
+
+		$this->merge([
+			'user' => $this->user
+		]);
+	}
 }
--- src/routes/app/provider.php
+++ src/routes/app/provider.php
@@ -1,33 +1,47 @@
 <?php
-// Route::group(['prefix' => '/provider', 'namespace' => 'Codificar\Geolocation\Http\Controllers', 'middleware' => 'auth.provider_api:api' ], function () {  
-Route::group(['prefix' => '/provider', 'namespace' => 'Codificar\Geolocation\Http\Controllers'], function () {  
-    //PLACES   
-    Route::get('/get_address_string', 'GeolocationController@getAddressByString');
+//Only or not Mobilidade
+Route::group(['prefix' => '/api/v3/provider', 'namespace' => 'Codificar\Geolocation\Http\Controllers', 'middleware' => 'auth.provider_api:api'], function () {
+	//PLACES   
+	Route::get('/get_address_string', 'GeolocationController@getAddressByString');
 
-    //Only Services
-    Route::post('/geolocation/get_polyline', 'DirectionsController@getPolylineAndEstimateByDirectionsApi');       
+	//Only Services
+	Route::post('/geolocation/get_polyline', 'DirectionsController@getPolylineAndEstimateByDirectionsApi');
 
-    //Get Geocode By PlaceId
-    Route::post('/get_place_details', 'GeolocationController@geocodeByPlaceId');
-    //Get Geocode Reverse
-    Route::post('/geolocation/get_address_by_geocode', 'GeolocationController@geocodeReverse');
-    //Get Geocode
-    Route::post('/geolocation/get_geocode_by_address', 'GeolocationController@geocode');   
+	//Get Geocode By PlaceId
+	Route::post('/get_place_details', 'GeolocationController@geocodeByPlaceId');
+	//Get Geocode Reverse
+	Route::post('/geolocation/get_address_by_geocode', 'GeolocationController@geocodeReverse');
+	//Get Geocode
+	Route::post('/geolocation/get_geocode_by_address', 'GeolocationController@geocode');
 });
 
 //Only Fretes
-Route::group(['prefix' => '/api/v1/provider', 'namespace' => 'Codificar\Geolocation\Http\Controllers', 'middleware' => 'auth.provider_api:api' ], function () {  
-//Direction       
-    Route::get('/geolocation/get_polyline', 'DirectionsController@getAddressByString'); 
+Route::group(['prefix' => '/api/v1/provider', 'namespace' => 'Codificar\Geolocation\Http\Controllers', 'middleware' => 'auth.provider_api:api'], function () {
+	//Direction       
+	Route::get('/geolocation/get_polyline', 'DirectionsController@getAddressByString');
 
-    //PLACES   
-    Route::get('/get_address_string', 'GeolocationController@getAddressByString');
-    
-    //Get Geocode By PlaceId
-    Route::post('/get_place_details', 'GeolocationController@geocodeByPlaceId');
-    //Get Geocode Reverse
-    Route::post('/geolocation/get_address_by_geocode', 'GeolocationController@geocodeReverse');
-    //Get Geocode
-    Route::post('/geolocation/get_geocode_by_address', 'GeolocationController@geocode');  
+	//PLACES   
+	Route::get('/get_address_string', 'GeolocationController@getAddressByString');
+
+	//Get Geocode By PlaceId
+	Route::post('/get_place_details', 'GeolocationController@geocodeByPlaceId');
+	//Get Geocode Reverse
+	Route::post('/geolocation/get_address_by_geocode', 'GeolocationController@geocodeReverse');
+	//Get Geocode
+	Route::post('/geolocation/get_geocode_by_address', 'GeolocationController@geocode');
 });
- 
+
+Route::group(['prefix' => '/provider', 'namespace' => 'Codificar\Geolocation\Http\Controllers'], function () {
+	//PLACES   
+	Route::get('/get_address_string', 'GeolocationController@getAddressByString');
+
+	//Only Services
+	Route::post('/geolocation/get_polyline', 'DirectionsController@getPolylineAndEstimateByDirectionsApi');
+
+	//Get Geocode By PlaceId
+	Route::post('/get_place_details', 'GeolocationController@geocodeByPlaceId');
+	//Get Geocode Reverse
+	Route::post('/geolocation/get_address_by_geocode', 'GeolocationController@geocodeReverse');
+	//Get Geocode
+	Route::post('/geolocation/get_geocode_by_address', 'GeolocationController@geocode');
+});
