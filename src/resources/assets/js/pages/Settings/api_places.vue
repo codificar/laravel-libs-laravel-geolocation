5f77afdcc4f9edc95868cf90ee157d092ca1d677
oto.luz <oto.luz@codificar.com.br>
2021-09-29 20:08:05 -0300

    log

--- src/resources/assets/js/pages/Settings/api_places.vue
+++ src/resources/assets/js/pages/Settings/api_places.vue
@@ -168,7 +168,7 @@ export default {
   },
   async mounted() {   
     const optionsList = JSON.parse(this.enumData)
-    console.log('teste_oto',optionsList);
+    console.log('teste_oto',JSON.stringify(optionsList));
     this.placesDataModel = JSON.parse(this.model)   
     this.placesOptions = optionsList.places_provider
     this.refreshOptions = optionsList.refresh_session_deflate_search
