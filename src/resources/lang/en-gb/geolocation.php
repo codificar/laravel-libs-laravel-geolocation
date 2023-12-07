010a8888cfb52f440fef216999cd5cc9154f3ce4
rafael.rehfeld <rafael.rehfeld@codificar.com.br>
2023-12-07 10:50:28 -0300

    corrigindo arquivo de tradução en-gb

--- src/resources/lang/en-gb/geolocation.php
+++ src/resources/lang/en-gb/geolocation.php
@@ -7,12 +7,12 @@ return [
      */
     "api_places" => "API Places",
     "api_places_provider" => "Service provider",
-    "api_places_url" => "server URL",
+    "api_places_url" => "Server URL",
     "api_places_key" => "Authentication key",
     "api_places_id" => "Application ID",
 
     "enable_red" => "Do you want to enable redundancy in the query:",
-    "yes" => "yes",
+    "yes" => "Yes",
     "no" => "No",
 
     "red_api_places_provider" => "Redundancy service provider",
@@ -27,20 +27,20 @@ return [
     "api_maps" => "Map Provider",
     "api_maps_provider" => "Service provider",
     "api_maps_key" => "Authentication key",
-    "unnamed_road" => "Rota sem nome",
+    "unnamed_road" => "Unnamed Road",
 
-    "invalid_url" => "This field must be a valid url",
+    "invalid_url" => "This field must be a valid URL",
     "invalid_require" => "Fill in this field",
 
     "optimize_route" => "Enable route optimization?",
 
-    "matrix_distance" => "Habilitar la búsqueda a través de la distancia de la matriz api",
-    "matrix_distance_redundancy" => "Habilite la búsqueda a través de la distancia de matriz api para redundancia",
+    "matrix_distance" => "Enable search through matrix API distance",
+    "matrix_distance_redundancy" => "Enable search through matrix API distance for redundancy",
 
     "no_data_found" => "No data found for this search.",
-    "place_is_required" => "Tracho do local é obrigatório",
-    "place_id_required" => "O Place ID é obrigatório",
-    "token_required" => "Preencha or Token",
+    "place_is_required" => "Place track is required",
+    "place_id_required" => "Place ID is required",
+    "token_required" => "Fill in the Token",
     "token_invalid" => "Invalid token",
     "token_string" => "Token must be a text",
     "id_required" => "Preset or ID",
@@ -48,21 +48,21 @@ return [
     "address_required" => "Preset or Straighten",
     "address_string" => "Address must be a string",
     'waypoints_required' => 'You are broken if necessary',
-    "latitude_and_longitude_equals_zero" => "A latitude and a longitude are equal to zero!",
+    "latitude_and_longitude_equals_zero" => "Latitude and longitude are equal to zero!",
     'incorrect_object_format' => 'Incorrect or incomplete broken points',
 
-    'minutes' => "Minute (s)",
-    "km" => "Km (s)",
-    'mile' => "Milha (s)",
+    'minutes' => "Minute(s)",
+    "km" => "Km(s)",
+    'mile' => "Mile(s)",
 
-    "settings_title" => "Configurações",
+    "settings_title" => "Settings",
 
     "refresh_session_deflate" => "Update session token on empty search",
 
-    "sessiontoken_work" => "¿Funcionalidad de token de sesión habilitada?",
+    "sessiontoken_work" => "Is session token functionality enabled?",
 
     'scale_validation_integer' => "Scale must be an integer.",
-    'width_validation_required' =>"Width is required.",
+    'width_validation_required' => "Width is required.",
     'width_validation_integer' => "Width must be an integer.",
     'height_validation_required' => "Height is required.",
     'height_validation_integer' => "Height must be an integer.",
@@ -70,4 +70,6 @@ return [
     'path_validation_array' => "Path points must be encapsulated inside an array.",
     'center_validation_required_without' => "Center is required when markers and/or path aren't present.",
     'zoom_validation_integer' => "Center is required when markers aren't present.",
+    'shortest_distance' => "Always opt for the shortest route?",
+
 ];
